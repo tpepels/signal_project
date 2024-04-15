f92c40c509c3a04cf817a5d93efd6914fa870543
Filip Sulich <sulich.f@gmail.com>
2024-04-15 17:03:34 +0200

    more javadocs created

--- src/main/java/com/cardiogenerator/outputs/OutputStrategy.java
+++ src/main/java/com/cardiogenerator/outputs/OutputStrategy.java
@@ -1,5 +1,14 @@
 package com.cardiogenerator.outputs;
 
+/**
+ * Define the output method for the OutputStrategy interface
+ */
 public interface OutputStrategy {
+    /**
+     * @param patientId - the ID of the patient
+     * @param timestamp - the timestamp of the data
+     * @param label - the label of the data
+     * @param data - the data to output
+     */
     void output(int patientId, long timestamp, String label, String data);
 }
