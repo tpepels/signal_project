c648fdbe99d3da437e75b0b6ff5a050378bba822
Kanilok <kammotyl@gmail.com>
2024-04-15 17:06:33 +0200

    added javadoc to PatientDataGenerator

--- src/main/java/com/cardiogenerator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardiogenerator/generators/PatientDataGenerator.java
@@ -2,6 +2,13 @@ package com.cardiogenerator.generators;
 
 import com.cardiogenerator.outputs.OutputStrategy;
 
+/**
+ * defines the PatientDataGenerator with only one method - generate()
+ */
 public interface PatientDataGenerator {
+    /**
+     * @param patientId
+     * @param outputStrategy
+     */
     void generate(int patientId, OutputStrategy outputStrategy);
 }
