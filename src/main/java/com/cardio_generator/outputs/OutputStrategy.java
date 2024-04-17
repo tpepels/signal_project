4f51ff650169f59f5a30c782942094f55f0d4dbe
noamfavier <n.favier@student.maastrichtuniversity.nl>
2024-04-17 16:35:57 +0200

    Added javadoc for OutputStrategy.java

--- src/main/java/com/cardio_generator/outputs/OutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/OutputStrategy.java
@@ -1,5 +1,17 @@
 package com.cardio_generator.outputs;
 
+/**
+ * This interface defines the method to output patient data.
+ */
 public interface OutputStrategy {
+
+    /**
+     * Outputs the patient data.
+     *
+     * @param patientId the patient ID
+     * @param timestamp the timestamp of the data
+     * @param label the label of the data
+     * @param data the data to output
+     */
     void output(int patientId, long timestamp, String label, String data);
 }
