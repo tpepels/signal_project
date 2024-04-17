1f2e1859d7fa458d95d75e0cba8f02cd6be0853b
Mathieu Kircher <mathieu.kircher@gmail.com>
2024-04-17 16:38:57 +0200

    Documented the TcpOutputStrategy.java file

--- src/main/java/com/cardio_generator/outputs/TcpOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/TcpOutputStrategy.java
@@ -6,12 +6,20 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.Executors;
 
+/**
+ * Is used to connect and output data to a client server
+ */
 public class TcpOutputStrategy implements OutputStrategy {
 
     private ServerSocket serverSocket;
     private Socket clientSocket;
     private PrintWriter out;
 
+    /**
+     * Connects the client socket output stream to the out variable.
+     * 
+     * @param port an integer refering to a valid port on which the server can be started
+     */
     public TcpOutputStrategy(int port) {
         try {
             serverSocket = new ServerSocket(port);
@@ -33,6 +41,14 @@ public class TcpOutputStrategy implements OutputStrategy {
     }
 
     @Override
+    /**
+     * Outputs patient information through the client socket
+     * 
+     * @param patientId an integer that identifies the specific patient
+     * @param timestamp a long that identifies the timestamp for the transfer
+     * @param label a string that indicates the label of the output
+     * @param data a string that contains the content of the output
+     */
     public void output(int patientId, long timestamp, String label, String data) {
         if (out != null) {
             String message = String.format("%d,%d,%s,%s", patientId, timestamp, label, data);
