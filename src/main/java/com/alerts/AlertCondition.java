3afbc02be42135ce59c04ea7f31994a35bcdf4ce
Mathieu Kircher <mathieu.kircher@gmail.com>
2024-05-12 19:54:20 +0200

    Commented AlertCondition

--- src/main/java/com/alerts/AlertCondition.java
+++ src/main/java/com/alerts/AlertCondition.java
@@ -5,5 +5,12 @@ import java.util.List;
 import com.data_management.Patient;
 
 public interface AlertCondition {
+
+    /**
+     * Returns a list of the relevant alerts for the patient
+     * 
+     * @param patient the data of the patient
+     * @return a list of alerts
+     */
     public List<Alert> checkCondition (Patient patient);
 }
