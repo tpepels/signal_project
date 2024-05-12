1b9360e3ed79bc8ae96e8f5799ae3a585f21ad95
noamfavier <n.favier@student.maastrichtuniversity.nl>
2024-05-12 21:05:58 +0200

    Added Main class for argument handling in maven for defining which class to run ✨

--- src/main/java/com/Main/Main.java
+++ src/main/java/com/Main/Main.java
@@ -1,4 +1,28 @@
 package com.Main;
 
+import com.cardio_generator.HealthDataSimulator;
+import com.data_management.CholesterolReader;
+import com.data_management.DataStorage;
+
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        if (args.length > 0) {
+            switch (args[0]) {
+                case "DataStorage":
+                    DataStorage.main(new String[]{});
+                    break;
+                case "CholesterolReader":
+                    CholesterolReader.main(new String[]{});
+                    break;
+                case "HealthDataSimulator":
+                    HealthDataSimulator.main(new String[]{});
+                    break;
+                default:
+                    System.out.println("Invalid argument. Please provide one of the following arguments: DataStorage, CholesterolReader, HealthDataSimulator");
+                    break;
+            }
+        }
+    }
 }
